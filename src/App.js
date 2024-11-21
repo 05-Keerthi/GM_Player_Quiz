daabc7bde146da98c73fa336d0afd7ecb8c1d403
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2024-11-21 18:04:54 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // App.js
 import React, { useContext } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { AuthContext } from "./context/AuthContext";
@@ -11,17 +14,20 @@ export default function App() {
   const { isAuthenticated } = useContext(AuthContext);
 
   return (
-    <Routes>
-      <Route
-        path="/login"
-        element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
-      />
-      <Route
-        path="/register"
-        element={isAuthenticated ? <Navigate to="/" /> : <RegisterPage />}
-      />
-      <Route path="/" element={<Home />} />
-      <Route path="/user/profile" element={<ProfilePage />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={isAuthenticated ? <Navigate to="/" /> : <RegisterPage />}
+        />
+        <Route path="/" element={<Home />} />
+        <Route path="/user/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
   );
 }
