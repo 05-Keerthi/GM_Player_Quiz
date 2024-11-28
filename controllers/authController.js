3bd115906adc33a0f20ffe0d9f200c992a59c1bc
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2024-11-28 15:54:20 +0530

    imported bacllistToekn in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const { sendWelcomeEmail } = require("../services/mailService");
-const { generateAccessToken, generateRefreshToken, verifyToken } = require("../services/authService");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyToken,
+} = require("../services/authService");
 const RefreshToken = require("../models/RefreshToken");
+const BlacklistedToken = require("../models/BlacklistedToken");
 
 // Register a new user
 const register = async (req, res) => {
@@ -11,17 +16,33 @@ const register = async (req, res) => {
 
     // Check username, email, and mobile existence
     if (await User.findOne({ username })) {
-      return res.status(400).json({ field: "username", message: "Username is already registered" });
+      return res
+        .status(400)
+        .json({ field: "username", message: "Username is already registered" });
     }
     if (await User.findOne({ email })) {
-      return res.status(400).json({ field: "email", message: "Email is already registered" });
+      return res
+        .status(400)
+        .json({ field: "email", message: "Email is already registered" });
     }
     if (await User.findOne({ mobile })) {
-      return res.status(400).json({ field: "mobile", message: "Phone number is already registered" });
+      return res
+        .status(400)
+        .json({
+          field: "mobile",
+          message: "Phone number is already registered",
+        });
     }
 
     // Create new user
-    const user = new User({ username, email, password, tenantId, mobile, role });
+    const user = new User({
+      username,
+      email,
+      password,
+      tenantId,
+      mobile,
+      role,
+    });
     await user.save();
 
     // Generate tokens
@@ -29,7 +50,10 @@ const register = async (req, res) => {
     const refreshToken = generateRefreshToken(user);
 
     // Store refresh token
-    const refreshTokenInstance = new RefreshToken({ token: refreshToken, userId: user._id });
+    const refreshTokenInstance = new RefreshToken({
+      token: refreshToken,
+      userId: user._id,
+    });
     await refreshTokenInstance.save();
 
     // Send Welcome Email
@@ -47,7 +71,8 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email }).populate("tenantId");
 
-    if (!existingUser) return res.status(400).json({ message: "Invalid Email." });
+    if (!existingUser)
+      return res.status(400).json({ message: "Invalid Email." });
 
     const isMatch = await bcrypt.compare(password, existingUser.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid Password." });
@@ -56,7 +81,10 @@ const login = async (req, res) => {
     const refreshToken = generateRefreshToken(existingUser);
 
     // Store refresh token
-    const refreshTokenInstance = new RefreshToken({ token: refreshToken, userId: existingUser._id });
+    const refreshTokenInstance = new RefreshToken({
+      token: refreshToken,
+      userId: existingUser._id,
+    });
     await refreshTokenInstance.save();
 
     res.status(200).json({
@@ -90,7 +118,9 @@ const refreshToken = async (req, res) => {
     // Check if the token exists in the database
     const storedToken = await RefreshToken.findOne({ token: refresh_token });
     if (!storedToken) {
-      return res.status(401).json({ message: "Invalid or expired refresh token." });
+      return res
+        .status(401)
+        .json({ message: "Invalid or expired refresh token." });
     }
 
     // Fetch the user to get the latest details
@@ -106,15 +136,15 @@ const refreshToken = async (req, res) => {
     storedToken.expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // Extend expiry time
     await storedToken.save();
 
-    res.status(200).json({ 
-      token: newAccessToken, 
+    res.status(200).json({
+      token: newAccessToken,
       user: {
         id: user._id,
         username: user.username,
         email: user.email,
         role: user.role,
         tenantId: user.tenantId,
-      }
+      },
     });
   } catch (error) {
     res.status(401).json({ message: "Token validation failed." });
@@ -134,23 +164,21 @@ const logout = async (req, res) => {
   try {
     // Add the current token to blacklist
     const blacklistedToken = new BlacklistedToken({
-      token: req.token
+      token: req.token,
     });
     await blacklistedToken.save();
 
-    res.status(200).json({ message: 'Successfully logged out.' });
+    res.status(200).json({ message: "Successfully logged out." });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-
 const listUsers = async (req, res) => {
   try {
-    
     const users = await User.find({ role: "user" })
       .select("-password") // Exclude passwords from the response
-      .populate('tenantId'); // Populate tenantId field with tenant details
+      .populate("tenantId"); // Populate tenantId field with tenant details
 
     res.status(200).json(users);
   } catch (error) {
@@ -158,4 +186,11 @@ const listUsers = async (req, res) => {
   }
 };
 
-module.exports = { register, login, refreshToken,  getProfile, logout, listUsers };
+module.exports = {
+  register,
+  login,
+  refreshToken,
+  getProfile,
+  logout,
+  listUsers,
+};
