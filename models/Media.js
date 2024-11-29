c4e0f5fb53296722f8babe22bde51cf798788c8f
= <=>
2024-11-29 14:43:38 +0530

    added the changes in media

--- models/Media.js
+++ models/Media.js
@@ -1,22 +1,10 @@
 const mongoose = require('mongoose');
 
-const mediaSchema = new mongoose.Schema({
-  filename: {
-    type: String,
-    required: true,
-  },
-  path: {
-    type: String,
-    required: true,
-  },
-  mimetype: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
+const mediaSchema = new mongoose.Schema({ 
+  filename: { type: String, required: true },
+  path: { type: String, required: true },
+  mimetype: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Media', mediaSchema);
