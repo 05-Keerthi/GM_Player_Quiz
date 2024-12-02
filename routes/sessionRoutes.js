124cd9b20f89570064c23d3132b13fec41058df2
= <=>
2024-12-02 14:46:11 +0530

    added session in app.js

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -13,29 +13,29 @@ const {
 } = require('../controllers/sessionController');
 
 // Create a new session
-router.post('/api/sessions/:quizId/publiz', auth, isAdmin, createSession);
+router.post('/sessions/:quizId/publiz', auth, isAdmin, createSession);
 
 // Join a session
-router.post('/api/sessions/:joinCode/:sessionId/join', auth, joinSession);
+router.post('/sessions/:joinCode/:sessionId/join', auth, joinSession);
 
 // Get players who joined a session
-router.get('/api/sessions/:joinCode/:sessionId/players', auth, isAdmin, getSessionPlayers);
+router.get('/sessions/:joinCode/:sessionId/players', auth, isAdmin, getSessionPlayers);
 
 // Start the session
-router.post('/api/sessions/:joinCode/:sessionId/start', auth, isAdmin, startSession);
+router.post('/sessions/:joinCode/:sessionId/start', auth, isAdmin, startSession);
 
 // Fetch questions for a session
-router.get('/api/sessions/:joinCode/:sessionId/questions', auth, isAdmin, getSessionQuestions);
+router.get('/sessions/:joinCode/:sessionId/questions', auth, isAdmin, getSessionQuestions);
 
 // Route: Change a question in the session by joinCode, sessionId, and questionId
-router.post('/api/sessions/:joinCode/:sessionId/:questionId/change', auth, isAdmin, changeQuestionByCodeAndSession);
+router.post('/sessions/:joinCode/:sessionId/:questionId/change', auth, isAdmin, changeQuestionByCodeAndSession);
 
 // Route: Get the current question in the session by joinCode and sessionId
-router.get('/api/sessions/:joinCode/:sessionId/current-question', auth, isAdmin, getCurrentQuestionInSession);
+router.get('/sessions/:joinCode/:sessionId/current-question', auth, isAdmin, getCurrentQuestionInSession);
 
 
 // End the session 
-router.post('/api/sessions/:joinCode/:sessionId/end', auth, isAdmin, endSession);
+router.post('/sessions/:joinCode/:sessionId/end', auth, isAdmin, endSession);
 
 
 
