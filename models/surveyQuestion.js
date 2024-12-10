388cd69f0892ee210d5efdb0d3e3f09d415036ba
= <=>
2024-12-10 11:42:06 +0530

    added the survey question models

---
+++ models/surveyQuestion.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const SurveyQuestionSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    dimension: { type: String, required: true },
+    year: { type: String, required: true },
+    imageUrl: { type: mongoose.Schema.Types.ObjectId, ref: 'Media', required: true },
+    timer: { type: Number, default: 30 },
+    liveScoreboard: { 
+        correctCount: { type: Number, default: 0 }, 
+        incorrectCount: { type: Number, default: 0 }
+    },
+    answerOptions: [
+        { 
+            optionText: { type: String, required: true }, 
+            isCorrect: { type: Boolean, required: true }
+        }
+    ],
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('SurveyQuestion', SurveyQuestionSchema);
---
+++ models/surveyQuiz.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const SurveyquizSchema = new mongoose.Schema({
+  title: { type: String, required: false },
+  description: { type: String },
+  isPublic: { type: Boolean, default: true },
+  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }],
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SurveyQuestion' }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  status: { type: String, enum: ['draft', 'active', 'closed'], default: 'draft' },
+  duration: { type: Number, default: 60 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('SurveyQuiz', SurveyquizSchema);
