c09978e5b682a572350051ad5930bdee483f2ef9
= <=>
2024-12-11 09:58:46 +0530

    added the survey model for answer

--- models/surveyanswer.js
+++ models/surveyanswer.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const surveyAnswerSchema = new mongoose.Schema({
-  question: { type: mongoose.Schema.Types.ObjectId, ref: 'SurveyQuestion', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  session: { type: mongoose.Schema.Types.ObjectId, ref: 'SurveySession', required: true },
-  answer: { type: mongoose.Schema.Types.Mixed, required: true },
+  surveyQuestion: { type: mongoose.Schema.Types.ObjectId, ref: 'SurveyQuestion', required: true },
+  surveyPlayers: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  surveySession: { type: mongoose.Schema.Types.ObjectId, ref: 'SurveySession', required: true },
+  surveyAnswer: { type: mongoose.Schema.Types.Mixed, required: true },
   timeTaken: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
