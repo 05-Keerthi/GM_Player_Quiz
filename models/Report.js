250325636a8af812062e843f168cc5c734d01fc1
= <=>
2024-12-13 16:17:21 +0530

    reports model

---
+++ models/Report.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const reportSchema = new mongoose.Schema({
+  quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  totalQuestions: { type: Number, required: true },
+  correctAnswers: { type: Number, required: true },
+  incorrectAnswers: { type: Number, required: true },
+  totalScore: { type: Number, required: true },
+  completedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Report', reportSchema);
