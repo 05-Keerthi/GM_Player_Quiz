13ae805eca490a471fa0af6aa8a6e611b510c20e
= <=>
2024-12-23 13:16:51 +0530

    reportsRoutes middlewares added changed

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,15 +1,13 @@
 // In your backend routes file
 const express = require("express");
 const router = express.Router();
+const { auth, isAdmin } = require('../middlewares/auth');
 const reportController = require("../controllers/reportController");
 
-router.get("/reports", reportController.getAllReports);
-router.get("/reports/user/:userId", reportController.getUserReports);
-router.get("/reports/:quizId", reportController.getReportByQuiz);
-router.get(
-  "/reports/:quizId/user/:userId",
-  reportController.getUserReportByQuiz
-);
-router.get("/reports/:quizId/stats", reportController.getQuizStats);
+router.get("/reports", auth, isAdmin, reportController.getAllReports);
+router.get("/reports/user/:userId", auth, reportController.getUserReports);
+router.get("/reports/:quizId", auth, isAdmin, reportController.getReportByQuiz);
+router.get("/reports/:quizId/user/:userId", auth, reportController.getUserReportByQuiz);
+router.get("/reports/:quizId/stats", auth, reportController.getQuizStats);
 
 module.exports = router;
