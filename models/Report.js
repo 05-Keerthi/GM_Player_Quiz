7a81472b3c26e1ef6a05a0808e53ead313782c95
= <=>
2024-12-26 16:22:00 +0530

    made changes in the report

--- models/Report.js
+++ models/Report.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const reportSchema = new mongoose.Schema({
   quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  sessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Session', required: true },
   totalQuestions: { type: Number, required: true },
   correctAnswers: { type: Number, required: true },
   incorrectAnswers: { type: Number, required: true },
