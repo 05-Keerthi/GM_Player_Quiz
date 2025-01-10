9c324aa96fc6bc70b493ad21fa860604a1bd167b
= <=>
2025-01-10 13:00:20 +0530

    removed the unwanted comments

--- models/question.js
+++ models/question.js
@@ -1,19 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const questionSchema = new mongoose.Schema({
-//   quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
-//   title: { type: String, required: true },
-//   type: { type: String, enum: ['multiple_choice', 'multiple_select', 'true_false', 'open_ended', 'poll'], required: true },
-//   imageUrl: { type: mongoose.Schema.Types.ObjectId, ref: 'Media', required: false },
-//   options: [{ text: String, isCorrect: Boolean }],
-//   correctAnswer: [{ type: String }],
-//   points: { type: Number, default: 10 },
-//   timer: { type: Number, default: 10 },
-// });
-
-// module.exports = mongoose.model('Question', questionSchema);
-
-
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
--- models/surveyQuestion.js
+++ models/surveyQuestion.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const SurveyQuestionSchema = new mongoose.Schema({
-//     title: { type: String, required: true },
-//     description: { type: String, required: true },
-//     dimension: { type: String },
-//     year: { type: String },
-//     imageUrl: { type: mongoose.Schema.Types.ObjectId, ref: 'Media' },
-//     timer: { type: Number, default: 30 },
-//     answerOptions: [
-//         {
-//             optionText: { type: String, required: true }, 
-//         }
-//     ],
-//     surveyQuiz: { type: mongoose.Schema.Types.ObjectId, ref: 'SurveyQuiz', required: true },
-//     createdAt: { type: Date, default: Date.now }, 
-// });
-
-// module.exports = mongoose.model('SurveyQuestion', SurveyQuestionSchema);
-
-
 const mongoose = require('mongoose');
 
 const SurveyQuestionSchema = new mongoose.Schema({
