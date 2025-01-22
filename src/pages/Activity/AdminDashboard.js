9d2c2d78adb95c2886cdc350a3a11e2185815fc0
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2025-01-22 11:34:27 +0530

    ui chages

--- src/pages/Activity/AdminDashboard.js
+++ src/pages/Activity/AdminDashboard.js
@@ -482,7 +482,7 @@ const AdminDashboard = () => {
           </div>
 
           {/* Stats Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             <StatsCard counts={activityData?.counts} />
             {/* <CustomCalendar /> */}
             <ProgressBars counts={activityData?.counts} />
