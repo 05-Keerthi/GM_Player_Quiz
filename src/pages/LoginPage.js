a38618ed0ce312ab87fd8afa396ddf9e0c4a1702
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2025-02-06 10:53:34 +0530

    update in css

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -158,7 +158,7 @@ const LoginPage = () => {
                 }}
                 className={`mt-1 block w-full rounded-md border ${
                   emailError ? "border-red-500" : "border-gray-300"
-                }px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500`}
+                } pl-4 px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500`}
                 placeholder="Enter your email"
               />
               {emailError && (
@@ -184,7 +184,7 @@ const LoginPage = () => {
                   }}
                   className={`mt-1 block w-full rounded-md border ${
                     passwordError ? "border-red-500" : "border-gray-300"
-                  }px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500`}
+                  }pl-4 px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500`}
                   placeholder="Enter your password"
                 />
                 <button
